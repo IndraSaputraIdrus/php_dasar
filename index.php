7b3b6876839e8143fbab5ee59af6ad888ddd749b
Ndrainz <indrasaputraidrus@gmail.com>
2022-01-12 20:34:09 +0800

    halaman home

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Daftar Siswa</title>
+</head>
+
+<body>
+   <h3>Daftar Siswa</h3>
+
+   <table border="1" cellpadding='10' cellspacing='0'>
+      <tr>
+         <th>#</th>
+         <th>Gambar</th>
+         <th>NRP</th>
+         <th>Nama</th>
+         <th>Email</th>
+         <th>Jurusan</th>
+         <th>Aksi</th>
+      </tr>
+      <tr>
+         <td>1</td>
+         <td><img src=""></td>
+         <td>202000001</td>
+         <td>Indra Saputra Idrus</td>
+         <td>indrasaputraidrus@gmail.com</td>
+         <td>Rekayasa Perangkat Lunak</td>
+         <td><a href="">Edit</a> | <a href="">delete</a></td>
+      </tr>
+   </table>
+</body>
+
+</html>
